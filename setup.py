f95e8057128f132be6ee8ac5ed5c419a6ead97f1
Rob S <schae234@gmail.com>
2016-02-10 11:47:09 -0600

    Try setuptools before trying distools. Force required packages to install

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 with open('README.rst') as file:
         long_description = file.read()
 
@@ -14,22 +18,27 @@ except LookupError:
 setup(
     name='powerlaw',
     py_modules=['powerlaw'],
-    version= '1.3.1',
+    version= '1.3.2',
     description='Toolbox for testing if a probability distribution fits a power law',
     long_description=long_description,
     author='Jeff Alstott',
     author_email='jeffalstott@gmail.com',
     url='http://www.github.com/jeffalstott/powerlaw',
-        requires=['scipy', 'numpy', 'matplotlib', 'mpmath'],
-        license='MIT',
-        classifiers=[
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Operating System :: OS Independent',
-            'Topic :: Scientific/Engineering :: Mathematics',
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Science/Research'
-        ]
+    install_requires=[
+        'numpy', 
+        'scipy', 
+        'matplotlib', 
+        'mpmath'
+    ],
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research'
+    ]
 )
